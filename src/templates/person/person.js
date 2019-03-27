58b63a4d83ca6880406c187781fc1fd177415855
Helen C <helen.chapman@torchbox.com>
2019-03-27 10:25:10 +0000

    Fall back to empty intro if there is no alt intro and no intro set for a person

--- src/templates/person/person.js
+++ src/templates/person/person.js
@@ -17,7 +17,7 @@ class PersonPage extends React.Component {
     this.title = `${props.firstName + ' ' + props.lastName ||
       ''} [${props.intro || ''}]`
     this.altTitle = `${props.firstName + ' ' + props.lastName ||
-      ''} [${props.altIntro || props.intro}]`
+      ''} [${props.altIntro || props.intro || ''}]`
     this.state = { title: this.title }
   }
 
