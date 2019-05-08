63633ddfe2969a258b97d489be81610afb718aa0
Tomasz Knapik <tomasz.knapik@torchbox.com>
2019-05-08 10:49:19 +0100

    Use document API in the useEffect

--- src/components/rich-text/rich-text.js
+++ src/components/rich-text/rich-text.js
@@ -1,27 +1,32 @@
 // Vendor Modules
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { pageUrl } from '@utils/urls'
 
-const RichText = ({ value }) => {
-  const el = document.createElement( 'body' )
-  el.innerHTML = value
+const RichText = (props) => {
+  const [newValue, setNewValue] = useState(props.value)
 
-  // Set "href" for page links.
-  for (const link of el.querySelectorAll('a[data-page-slug][data-page-type]')) {
-    const url = pageUrl({
-      type: link.dataset.pageType,
-      slug: link.dataset.pageSlug,
-      serviceSlug: link.dataset.pageServiceSlug
-    })
-    link.href = url
-    delete link.dataset.pageType
-    delete link.dataset.pageSlug
-    delete link.dataset.pageServiceSlug
-  }
+  // Need to encompass document on the runtime only.
+  useEffect(() => {
+    const el = document.createElement( 'body' )
+    el.innerHTML = props.value
 
-  const newValue = el.innerHTML
+    // Set "href" for page links.
+    for (const link of el.querySelectorAll('a[data-page-slug][data-page-type]')) {
+      const url = pageUrl({
+        type: link.dataset.pageType,
+        slug: link.dataset.pageSlug,
+        serviceSlug: link.dataset.pageServiceSlug
+      })
+      link.href = url
+      delete link.dataset.pageType
+      delete link.dataset.pageSlug
+      delete link.dataset.pageServiceSlug
+    }
+    setNewValue(el.innerHTML)
+  })
+
 
   return <div dangerouslySetInnerHTML={{ __html: newValue }} />
 }
