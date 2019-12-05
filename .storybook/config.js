c3810c6dca29f4f139de94c484298adcac03e81b
Thibaud Colas <thibaudcolas@gmail.com>
2019-12-05 15:44:25 +0000

    Update Storybook - Gatsby integration config for v5 recommendations from Gatsby

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,27 @@
-import { addDecorator } from '@storybook/react'; // <- or your storybook framework
-import { withBackgrounds } from '@storybook/addon-backgrounds';
-import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react'
+import { withBackgrounds } from '@storybook/addon-backgrounds'
+import { configure } from '@storybook/react'
 import 'reset-css'
 import '../src/styles/_fonts.scss'
 
+/**
+ * Storybook configuration overrides for Gatsby compatibility.
+ * See https://www.gatsbyjs.org/docs/visual-testing-with-storybook/.
+ */
+
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
+configure(require.context('../src', true, /\.stories\.js$/), module)
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+}
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = ''
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname)
 }
 
 addDecorator(
@@ -17,18 +31,3 @@ addDecorator(
     { name: 'white', value: '#FFF' },
   ])
 )
-
-// Gatsby's Link overrides:
-// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
-global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
-}
-// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
-global.__PATH_PREFIX__ = ""
-// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
-window.___navigate = pathname => {
-  action("NavigateTo:")(pathname)
-}
-configure(loadStories, module)
-
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path')
 
-module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = ({ config }) => {
   // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-  defaultConfig.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
-
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  defaultConfig.module.rules[0].use[0].loader = require.resolve("babel-loader")
-
-  // use @babel/plugin-proposal-class-properties for class arrow functions
-  defaultConfig.module.rules[0].use[0].options.plugins = [
-    require.resolve("@babel/plugin-proposal-class-properties"),
-    // require.resolve("@babel/plugin-syntax-export-default-from")
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
+  // use @babel/preset-react for JSX and env (instead of staged presets)
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
   ]
+  config.module.rules[0].use[0].options.plugins = [
+    // use @babel/plugin-proposal-class-properties for class arrow functions
+    require.resolve('@babel/plugin-proposal-class-properties'),
+    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    require.resolve('babel-plugin-remove-graphql-queries'),
+  ]
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ['browser', 'module', 'main']
 
   // SCSS Support
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.scss$/,
     loaders: [
-      "style-loader",
+      'style-loader',
       {
         loader: 'css-loader',
         options: {
@@ -25,18 +31,10 @@ module.exports = (baseConfig, env, defaultConfig) => {
           importLoaders: 1,
         },
       },
-      "sass-loader"
+      'sass-loader',
     ],
-    include: path.resolve(__dirname, "../src/")
+    include: path.resolve(__dirname, '../src/'),
   })
 
-  // use @babel/preset-react for JSX and env (instead of staged presets)
-  defaultConfig.module.rules[0].use[0].options.presets = [
-    require.resolve("@babel/preset-react"),
-    require.resolve("@babel/preset-env"),
-  ]
-
-  defaultConfig.resolve.mainFields = ["browser", "module", "main"]
-
-  return defaultConfig
+  return config
 }
