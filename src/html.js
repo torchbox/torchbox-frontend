31029bc1b95523e7aa1283e7acf400e86233a38d
Storm Heg <storm.heg@torchbox.com>
2020-06-12 14:47:24 +0200

    Add tracking code provided by Autopilot

--- src/html.js
+++ src/html.js
@@ -12,6 +12,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+
+        {/* Autopilot tracking code */}
+        {/* eslint-disable-next-line */}
+        <script type="text/javascript">(function(o){var b="https://nimblebird.co/anywhere/",t="c357566a4da04797a3ce502245da20f0caa751e06fb84345885de2e3f659c6ee",a=window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}},c=encodeURIComponent,s="SCRIPT",d=document,l=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||""),j="text/javascript",z,y;if(!window.Autopilot) window.Autopilot=a;if(o.app) p="devmode=true&"+p;z=function(src,asy){var e=d.createElement(s);e.src=src;e.type=j;e.async=asy;l.parentNode.insertBefore(e,l);};y=function(){z(b+t+'?'+p,true);};if(window.attachEvent){window.attachEvent("onload",y);}else{window.addEventListener("load",y,false);}})({});</script>
+
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
