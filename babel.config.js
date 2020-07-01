dc26da11d10b04efac63ccd19c72ec0b5cf98213
Nathan <NathHorrigan@gmail.com>
2020-07-01 10:29:11 +0100

    Add babel plugin-proposal-optional-chaining

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
       ]
     ],
     "plugins": [
+      "@babel/plugin-proposal-optional-chaining",
       ["module-resolver", {
         "root": ["./"],
         "alias": pathMappings
